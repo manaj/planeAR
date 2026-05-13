cf0028f1b814631021fd0b64f0fe7df4acd9bd73
Manaj <manaj.chakrobarti@gmail.com>
2026-05-13 14:53:40 +0200

    changed size of the bird

--- planeAR/ARViewContainer.swift
+++ planeAR/ARViewContainer.swift
@@ -20,12 +20,12 @@ struct ARViewContainer : UIViewRepresentable {
     func updateUIView(_ uiView: ARView, context: Context) {
         // 1.
         //let anchorEntity = AnchorEntity(plane: .any)
-        let anchorEntity = AnchorEntity(world: [-1, -1, -3])
+        let anchorEntity = AnchorEntity(world: [0, -1, -20])
 
         // 2.
         guard let modelEntity = try? Entity.loadModel(named: modelName) else { return }
         
-        modelEntity.setScale(SIMD3<Float>(0.25, 0.25, 0.25), relativeTo: nil)
+        modelEntity.setScale(SIMD3<Float>(0.8, 0.8, 0.8), relativeTo: nil)
         // 3.
         anchorEntity.addChild(modelEntity)
         
